03c2b5d643bfb03d45e4b78109c22de536fefeae
Giovani Generali <giovani.generali@gmail.com>
2019-05-13 22:48:33 -0300

    add 401 root route

--- backend/start/routes.js
+++ backend/start/routes.js
@@ -2,6 +2,10 @@
 
 const Route = use('Route')
 
+Route.get('/', ({ response }) => {
+  return response.status(401).send()
+})
+
 Route.post('users', 'UserController.store').validator('User/Store')
 
 Route.post('sessions', 'SessionController.store').validator('Session')
