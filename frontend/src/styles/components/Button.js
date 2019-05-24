91b03d116c7c1210c275d9235e3395eb1b15ca63
Giovani Generali <giovani.generali@gmail.com>
2019-05-24 16:14:59 -0300

    update button

--- frontend/src/styles/components/Button.js
+++ frontend/src/styles/components/Button.js
@@ -5,17 +5,21 @@ const Button = styled.button.attrs(() => ({
 }))`
   border-radius: 3px;
   background: #e5556e;
-  transition: background-color 0.2s ease;
   border: 0;
   border-radius: 25px;
   color: #fff;
   font-size: 16px;
   font-weight: bold;
   height: 50px;
+  transition: background-color 0.2s ease, opacity 0.2s ease;
 
   &:hover,
   &:focus {
     background-color: #fc5e76;
+  }
+
+  &:disabled {
+    cursor: progress;
   }
 `;
 
